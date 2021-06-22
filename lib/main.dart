871a02862816e1ca9b034d3e5a5e181de4f29eb5
giih97 <gbamdev9@gmail.com>
2021-06-22 15:03:09 -0300

    criando a função main

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Container(
+      color: Colors.purple,
+      child: Center(
+          child: Container(
+        height: 200,
+        width: 200,
+        color: Colors.green,
+      )),
+    ),
+  )));
+}
